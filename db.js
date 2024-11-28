c594433c475bff8def38893cd746936c74c453de
Giovanni <99891128+GiovanniBri0nes@users.noreply.github.com>
2024-11-28 00:06:17 -0600

    Add files via upload

--- db.js
+++ db.js
@@ -2,10 +2,10 @@
 const sql = require('mssql');
 
 const config = {
-    user: 'sa', // Reemplaza con tu usuario de la base de datos
-    password: 'sas', // Reemplaza con tu contraseña de la base de datos
-    server: 'LAPTOP-03NQKEKK', // Reemplaza con tu servidor de la base de datos
-    database: 'PIA_PROG_WEB',
+    user: 'admin', // Reemplaza con tu usuario de la base de datos
+    password: '4tJ8y9Xm', // Reemplaza con tu contraseña de la base de datos
+    server: 'piaprogweb.cfuoywa2yrpx.us-east-2.rds.amazonaws.com', // Reemplaza con tu servidor de la base de datos
+    database: 'recipiente',
     options: {
         encrypt: true, // Usar solo si tu servidor de base de datos lo requiere
         enableArithAbort: true,
